c14e598c3e35d6819a4620a918500d2fcdde0a8e
sznorbee <norbert.szekeres@gmail.com>
2018-04-30 15:03:59 +0200

    first commit

---
+++ calculalert.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+
+        var a = [1,2,3,4,5,6,7,8,9,10];
+        var b = a;
+
+        var ra = a[Math.floor(Math.random() * a.length)];
+        var rb = b[Math.floor(Math.random() * b.length)];
+
+        var eredmeny = ra * rb;
+
+        var valasz = prompt("Mennyi: " + ra + " x " + rb + "?");
+
+        if (valasz == eredmeny) {
+            alert
+            (
+                "Ugyi bugyi vagy!\n " +
+                 ra + " x " + rb + " az tenyleg " + eredmeny + "\n"
+
+            );
+            console.log("ok if");
+        }else{
+            alert("Boccccs ez nem ok,\n a helyes valasz: " + eredmeny + "\n" + ra + " x " + rb + " = " + eredmeny);
+            console.log("else if");
+        }
+        console.log("after if");
+        
+        $("button").html("Uj kerdes");
+    });
+
+});
