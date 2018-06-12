d81ce5b430d6871f329140a3d90e5f1dd0f601de
sznorbee <norbert.szekeres@gmail.com>
2018-06-12 13:26:44 +0200

    alertless display works

--- js/calculalert.js
+++ js/calculalert.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+    console.log("jquery is ready");
 
-    $("#startButton").click(function()
+    $("#startButton").click(multiplicationTest);
+    $("#newButton").click(multiplicationTest);
+    
+    $("#stopButton").click(function() {
+        location.reload();
+    });
+
+    function multiplicationTest()
     {
-
+        
+        $("#result").addClass("hide");
+        $("#newOrStop").addClass("hide");
+ 
         var a = [1,2,3,4,5,6,7,8,9,10];
         var b = a;
 
@@ -14,6 +25,8 @@ $(document).ready(function(){
 
         //change display
         $("#startButton").addClass("hide");
+       
+        $("input[type=text]").val(" ");
         $("#question").removeClass("hide");
         $("h2").html("Mennyi: " + ra + " x " + rb + "?");
 
@@ -23,6 +36,9 @@ $(document).ready(function(){
         //submit form to get value from input
         $("form").submit(function(event)
         {
+            event.preventDefault();
+            $("#result").removeClass();
+            $("#result").addClass("hide");
             var valasz = $("input[type=text]").val();
             console.log("valasz " + valasz);
 
@@ -33,25 +49,34 @@ $(document).ready(function(){
                 //display message for good response
                 $("#question").addClass("hide");
                 $("#result").removeClass("hide");
+                $("#result").addClass("good");
                 $("h2").html("Ügyes vagy!");
-                $("p").html(ra + " x " + rb + " az tenyleg " + eredmeny + "\n");
+                $("p").html(ra + " x " + rb + "<br/> az tenyleg " + eredmeny);
                 console.log("Ok response");
             }else{
                     //alert("Boccccs ez nem ok,\n a helyes valasz: " + eredmeny + "\n" + ra + " x " + rb + " = " + eredmeny);
                     $("#question").addClass("hide");
                     $("#result").removeClass("hide");
+                    $("#result").addClass("bad");
                     $("h2").html("Ez NEM jó!");
-                    $("p").html("a helyes valasz: " + ra + " x " + rb + "= " + eredmeny + "\n");
+                    $("p").html("a helyes valasz:<br/> " + ra + " x " + rb + " = " + eredmeny);
                     console.log("nok response");
                 }
             
             //change display to new question or stop
             $("#question").addClass("hide");
-            console.log("new question ");
+            
+            $("#newOrStop").removeClass("hide");
+            $("h3").html("Szeretnél új kérdést?");
+            
+            console.log("end of function");
+            return false;
 
-            event.preventDefault();
+           
         });
+        console.log("outside of function");
         
-    });
-
+    }; //end of multipl func.
+    
 });
+console.log("outside of jquery");
